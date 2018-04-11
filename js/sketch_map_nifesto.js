685fb1bfcf9f56b7e2f40b4d73dc9d38d76d7d10
yèct <yectocsalev@gmail.com>
2018-04-11 05:49:17 -0400

    van cambios ( .

--- js/sketch_map_nifesto.js
+++ js/sketch_map_nifesto.js
@@ -1,24 +1,45 @@
 
 
 
-var sourceText = "le code et le mot respiren*t si on les garde en vie.";
+var sourceText = "[...] le code et le mot respirent si on les garde en vie.";
+var sourceText1 = "* auto-transformarción";
+var sourceText2 = "مشاركة الحياة ، والحفاظ على الاتحاد واحتضان كلمة";
+var sourceText3 = "Some thoughts have a certain sound [...]";
 
 function setup() {
+  textFont("Mikado");
   createCanvas(windowWidth, windowHeight);
 }
 function draw() {
   background(155,190,0);
-  fill(0);
-  
-  textSize(random(11, 13));
   textAlign(CENTER, CENTER);
   
   var middle = sourceText.length / 2;
-  var left = middle - ((mouseX / width) * middle);
-  var right = middle + ((mouseX / width) * middle);
-  text(
+  var left = middle - ((mouseY / 600) * middle);
+  var right = middle + ((mouseY / 600) * middle);
+  textSize(random(11, 24));
+   
+    translate(0, 100);
+
+    fill(random(100), 0, 200, random(200));
+    text(
     sourceText.substring(left, right+1),
-    width/2.8, height/2.8);
+    width/2, mouseY/5.35);
+
+    fill(255, 255, 255, random(200));
+    text(
+    sourceText1.substring(left, right+1),
+    width/2.3, mouseX/5);
+push();
+    fill(0, 0, 0, random(255, 60));
+    text(
+    sourceText2.substring(left, right+2),
+    width/1.7, mouseY/4.65);
+pop();
+    fill(0, 40, 155, random(200));
+    text(
+    sourceText3.substring(left, right+2),
+    width/2.6, mouseX/4.35);
 }
 
 
